d86495c68292b730867c44665e479574c76d3311
Dakuchi <datngthanh98@gmail.com>
2024-10-03 19:50:19 +0700

    deploy ec2 configuration using modules

--- main.tf
+++ main.tf
@@ -3,15 +3,6 @@ provider "aws" {
   region = "ap-southeast-1"
 }
 
-variable "avail_zone" {} 
-variable "vpc_cidr_block" {}
-variable "subnet_cidr_block" {}
-variable "env_prefix" {}
-variable "my_ip" {}
-variable "instance_type" {}
-variable "public_key_location" {}
-variable "private_key_location" {}
-
 ## Create recources
 resource "aws_vpc" "myapp-vpc" {
   cidr_block = var.vpc_cidr_block
@@ -20,32 +11,14 @@ resource "aws_vpc" "myapp-vpc" {
   }
 }
 
-## Create recources
-resource "aws_subnet" "myapp-subnet-1" {
+# Use module 
+module "myapp-subnet" {
+  source = "./modules/subnet"
+  subnet_cidr_block = var.subnet_cidr_block
+  avail_zone = var.avail_zone
+  env_prefix = var.env_prefix
   vpc_id = aws_vpc.myapp-vpc.id
-  cidr_block = var.subnet_cidr_block
-  availability_zone = var.avail_zone
-  tags = {
-    Name: "${var.env_prefix}-subnet-1"
-  }
-}
-
-resource "aws_internet_gateway" "myapp-igw" {
-  vpc_id = aws_vpc.myapp-vpc.id
-  tags = {
-    Name: "${var.env_prefix}-igw"
-  }
-}
-
-resource "aws_default_route_table" "main-rtb" {
   default_route_table_id = aws_vpc.myapp-vpc.default_route_table_id
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.myapp-igw.id
-  }
-  tags = {
-    Name: "${var.env_prefix}-main-rtb"
-  }
 }
 
 resource "aws_default_security_group" "default-sg" {
@@ -101,42 +74,19 @@ resource "aws_instance" "myapp-server" {
   ami = data.aws_ami.latest-amazon-linux-image.id
   instance_type = var.instance_type
 
-  subnet_id = aws_subnet.myapp-subnet-1.id
+  # Use the output from module as input
+  subnet_id = module.myapp-subnet.subnet.id
   vpc_security_group_ids = [aws_default_security_group.default-sg.id]
   availability_zone = var.avail_zone
 
   associate_public_ip_address = true
   key_name = aws_key_pair.ssh-key.key_name
 
-  # Create ssh connection to created server
-  connection {
-    type = "ssh"
-    host = self.public_ip
-    user = "ec2-user"
-    private_key = file(var.private_key_location)
-  }
-  # Copy local file to created server
-  provisioner "file" {
-    source = "entry-script.sh"
-    destination = "/home/ec2-user/entry-script-on-ec2.sh"
-  }
+  user_data = "${file("entry-script.sh")}"
 
-  # Output the public IP in local machine
-  provisioner "local-exec" {
-    command = "echo ${self.public_ip} > output.txt"
-  }
-  provisioner "remote-exec" {
-    inline = [ 
-      "export ENV=dev",
-      "mkdir newdir"
-     ]
-  }
   tags = {
     Name: "${var.env_prefix}-server"
   }
 }
 
-# Output EC2 public ip
-output "ec2_public_ip" {
-  value = aws_instance.myapp-server.public_ip
-}
+
